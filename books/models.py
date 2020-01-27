c26fafa31559c5a58de73ad3cae60737ed2f6aaf
thoap <thomas.apolke@posteo.de>
2020-01-27 22:19:17 +0100

    make a book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13)
+    
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
+    def __str__(self):
+        return self.title
